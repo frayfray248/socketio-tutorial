10c690ecaf0c63b1b593400bf48a639baa296653
frayfray248 <nicman248@hotmail.com>
2020-11-20 15:12:16 -0800

    hello world test

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const app = required('express');
+// requires
+const express = require('express');
 const http = require('http');
+
+// app and server creation
+const app = express();
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world!</h1>');
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
